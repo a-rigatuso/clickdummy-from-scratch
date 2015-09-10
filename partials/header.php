d80f150226b8426b155665149213b19a13606896
Alexander Rigatuso <a.rigatuso@lingner.com>
2015-09-11 00:17:13 +0200

    Add a few best practices from boilerplate

---
+++ partials/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html class="no-js" lang="de">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- <link rel="apple-touch-icon" href="apple-touch-icon.png"> -->
+
+        <link rel="stylesheet" href="css/style.css">
+        <!-- <script src="js/modernizr.js"></script> -->
+    </head>
+    <body>
